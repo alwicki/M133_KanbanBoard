519dc1656c765f2e634412c1aa87ed371c162b59
Aurel Wicki <aurellinus_wicki@sluz.ch>
2020-12-04 12:54:04 +0100

    improved dynamic generation of lanes

--- frontend/client.js
+++ frontend/client.js
@@ -1,6 +1,9 @@
 // position of task to create
 let position;
 
+// variable to save lanes from api
+let lanes;
+
 // focus modal if shown
 $('#taskModal').on('shown.bs.modal', function () {
   $('input').trigger('focus');
@@ -11,6 +14,7 @@ const form = document.querySelector('form');
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
   const task = {
+    id: null,
     description: document.querySelector('input').value,
     position: position,
   };
@@ -27,6 +31,60 @@ form.addEventListener('submit', async (event) => {
   $('#taskModal').modal('hide');
   await loadTasks();
 });
+
+async function loadLanes() {
+  const response = await fetch('/lanes');
+  lanes = await response.json();
+  const laneRow = document.getElementById('laneRow');
+  lanes.forEach((lane) => {
+    let laneCol = document.createElement('div');
+    laneCol.className = 'col bg-light vh-80 border-right';
+
+    let titelRow = document.createElement('div');
+    titelRow.className = 'row mb-4';
+    titelRow.style.backgroundColor = lane.color;
+
+    let titel = document.createElement('h2');
+    titel.className = 'text-light mx-auto';
+    titel.innerText = lane.titel;
+
+    let cardSpace = document.createElement('div');
+    cardSpace.id = lane.id;
+    cardSpace.className = 'h-100';
+    cardSpace.setAttribute('ondragover', 'allowDrop(event)');
+    cardSpace.setAttribute('ondrop', 'drop(event)');
+    cardSpace.style = ':drop';
+
+    let createBtn = document.createElement('button');
+    createBtn.innerHTML = "<img src='/assets/icons/plus.svg'>";
+    createBtn.className = 'btn btn-outline-dark mx-auto mb-4';
+    createBtn.onclick = () => {
+      position = lane.id;
+    };
+    createBtn.setAttribute('data-toggle', 'modal');
+    createBtn.setAttribute('data-target', '#taskModal');
+    let btnRow = document.createElement('row');
+    btnRow.className = 'row';
+    btnRow.appendChild(createBtn);
+
+    titelRow.appendChild(titel);
+
+    laneCol.appendChild(titelRow);
+    laneCol.appendChild(btnRow);
+    laneCol.appendChild(cardSpace);
+    laneRow.appendChild(laneCol);
+  });
+  loadTasks();
+}
+
+async function loadTasks() {
+  const response = await fetch('/tasks');
+  const tasks = await response.json();
+  lanes.forEach((lane) => (document.getElementById(lane.id).innerHTML = ''));
+  tasks.forEach((task) => {
+    document.getElementById(task.position).appendChild(createTaskCard(task));
+  });
+}
 
 function createTaskCard(task) {
   const card = document.createElement('div');
@@ -48,7 +106,7 @@ function createTaskCard(task) {
     cardBody.appendChild(createMoveBtn(task, -1));
   }
   cardBody.appendChild(deleteBtn);
-  if (task.position <= 1) {
+  if (task.position < lanes.length - 1) {
     cardBody.appendChild(createMoveBtn(task, 1));
   }
   card.appendChild(cardBody);
@@ -88,76 +146,8 @@ async function deleteTask(id) {
   await loadTasks();
 }
 
-async function loadLanes() {
-  const response = await fetch('/lanes');
-  const lanes = await response.json();
-  const laneRow = document.getElementById('laneRow');
-  lanes.forEach((lane) => {
-    let laneCol = document.createElement('div');
-    laneCol.className = 'col bg-light vh-80 border-right';
-
-    let titelRow = document.createElement('div');
-    titelRow.className = 'row mb-4';
-    titelRow.style.backgroundColor = lane.color;
-
-    let titel = document.createElement('h2');
-    titel.className = 'text-light mx-auto';
-    titel.innerText = lane.titel;
-
-    let cardSpace = document.createElement('div');
-    cardSpace.id = lane.id;
-    cardSpace.className = 'h-100';
-    cardSpace.setAttribute('ondragover', 'allowDrop(event)');
-    cardSpace.setAttribute('ondrop', 'drop(event)');
-
-    let createBtn = document.createElement('button');
-    createBtn.innerHTML = "<img src='/assets/icons/plus.svg'>";
-    createBtn.className = 'btn btn-outline-dark mx-auto mb-4';
-    createBtn.onclick = () => {
-      position = lane.id;
-    };
-    createBtn.setAttribute('data-toggle', 'modal');
-    createBtn.setAttribute('data-target', '#taskModal');
-    let btnRow = document.createElement('row');
-    btnRow.className = 'row';
-    btnRow.appendChild(createBtn);
-
-    titelRow.appendChild(titel);
-
-    laneCol.appendChild(titelRow);
-    laneCol.appendChild(btnRow);
-    laneCol.appendChild(cardSpace);
-    laneRow.appendChild(laneCol);
-  });
-  loadTasks();
-}
-
-async function loadTasks() {
-  const response = await fetch('/tasks');
-  const tasks = await response.json();
-  const todo = document.getElementById('0');
-  const inprogress = document.getElementById('1');
-  const done = document.getElementById('2');
-  todo.innerHTML = '';
-  inprogress.innerHTML = '';
-  done.innerHTML = '';
-  tasks.forEach((task) => {
-    switch (task.position) {
-      case 0:
-        todo.appendChild(createTaskCard(task));
-        break;
-      case 1:
-        inprogress.appendChild(createTaskCard(task));
-        break;
-      default:
-        done.appendChild(createTaskCard(task));
-        break;
-    }
-  });
-}
-
 function allowDrop(ev) {
-  ev.preventDefault();
+  if (ev.target.id) ev.preventDefault();
 }
 
 function drag(ev) {
@@ -169,6 +159,7 @@ function drop(ev) {
   var data = ev.dataTransfer.getData('text');
   changePosition(data, ev.target.id);
 }
+
 async function changePosition(taskId, position) {
   var description = document.getElementById(taskId).querySelector('.card-title')
     .innerHTML;
